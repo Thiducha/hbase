f44782e09c995d0405eb08dadcb199f960d7912d
Nicolas Liochon <nkeywal@gmail.com>
2012-09-24 16:39:29 +0200

    Merge branch 'clean' into 6364
    Conflicts:
    	hbase-server/src/main/java/org/apache/hadoop/hbase/ipc/ClientCache.java

---
+++ hbase-common/src/test/java/org/apache/hadoop/hbase/ResourceChecker.java
@@ -0,0 +1,166 @@
+/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.hadoop.hbase;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import java.util.*;
+
+
+public class ResourceChecker {
+  private static final Log LOG = LogFactory.getLog(ResourceChecker.class);
+  private String tagLine;
+
+  public ResourceChecker(String tagLine) {
+    this.tagLine = tagLine;
+  }
+
+
+  abstract static class ResourceAnalyzer {
+    /**
+     * Maximum we set for the resource. Will get a warning in logs
+     * if we go other this limit.
+     */
+    public int getMax() {
+      return Integer.MAX_VALUE;
+    }
+
+    public int getMin() {
+      return Integer.MIN_VALUE;
+    }
+
+    public String getName() {
+      String className = this.getClass().getSimpleName();
+      final String extName = ResourceAnalyzer.class.getSimpleName();
+      if (className.endsWith(extName)) {
+        return className.substring(extName.length());
+      } else {
+        return className;
+      }
+    }
+
+    abstract public int getVal();
+  }
+
+
+  private List<ResourceAnalyzer> ras = new ArrayList<ResourceAnalyzer>();
+  private int[] initialValues;
+  private int[] endingValues;
+
+
+  public void fillInit() {
+    initialValues = new int[ras.size()];
+    fill(initialValues);
+  }
+
+  public void fillEndings() {
+    endingValues = new int[ras.size()];
+    fill(endingValues);
+  }
+
+  private void fill(int[] vals) {
+    int i = 0;
+    for (ResourceAnalyzer ra : ras) {
+      vals[i++] = ra.getVal();
+    }
+  }
+
+  public void checkInit() {
+    check(initialValues);
+  }
+
+  public void checkEndings() {
+    check(endingValues);
+  }
+
+  public void check(int[] vals) {
+    int i = 0;
+    for (ResourceAnalyzer ra : ras) {
+      int cur = vals[i++];
+      if (cur < ra.getMin()) {
+        LOG.warn(ra.getName() + ": " + cur + " is inferior to " + ra.getMin());
+      }
+      if (cur > ra.getMax()) {
+        LOG.warn(ra.getName() + ": " + cur + " is superior to " + ra.getMax());
+      }
+    }
+  }
+
+  public void logInit() {
+    log(initialValues);
+  }
+
+  public void logEnding() {
+    int i = 0;
+    StringBuilder sb = new StringBuilder();
+    for (ResourceAnalyzer ra : ras) {
+      int curP = initialValues[i++];
+      int curN = endingValues[i++];
+      if (sb.length() > 0) sb.append(", ");
+      sb.append(ra.getName()).append("= ").append(curN).append(" was (").append(curP).append(")");
+      if (curN > curP) {
+        sb.append(" - ").append(ra.getName()).append(" LEAK? -");
+      }
+    }
+    LOG.info("after: " + tagLine + " " + sb);
+  }
+
+  public void log(int[] vals) {
+    int i = 0;
+    StringBuilder sb = new StringBuilder();
+    for (ResourceAnalyzer ra : ras) {
+      int cur = vals[i++];
+      if (sb.length() > 0) sb.append(", ");
+      sb.append(ra.getName()).append("= ").append(cur);
+    }
+    LOG.info("before: " + tagLine + " " + sb);
+  }
+
+
+  public void start() {
+    if (ras.size() == 0) {
+      LOG.info("No resource analyzer");
+      return;
+    }
+    fillInit();
+    logInit();
+    checkInit();
+  }
+
+  public void end() {
+    if (ras.size() == 0) {
+      LOG.info("No resource analyzer");
+      return;
+    }
+    if (initialValues == null) {
+      LOG.warn("No initial values");
+      return;
+    }
+
+    fillEndings();
+    logEnding();
+    checkEndings();
+  }
+
+  public void addResourceAnalyzer(ResourceAnalyzer ra) {
+    ras.add(ra);
+  }
+}
---
+++ hbase-common/src/test/java/org/apache/hadoop/hbase/ResourceCheckerJUnitListener.java
@@ -0,0 +1,127 @@
+package org.apache.hadoop.hbase;
+
+
+import com.sun.management.UnixOperatingSystemMXBean;
+import org.junit.runner.notification.RunListener;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.OperatingSystemMXBean;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ResourceCheckerJUnitListener extends RunListener {
+  private String processId = ManagementFactory.getRuntimeMXBean().getName();
+
+  private Map<String, ResourceChecker> rcs = new HashMap<String, ResourceChecker>();
+
+  static class ThreadResourceAnalyzer extends ResourceChecker.ResourceAnalyzer {
+    public int getVal() {
+      return Thread.getAllStackTraces().size();
+    }
+
+    public int getMax() {
+      return 500;
+    }
+  }
+
+  /**
+   * On unix, we know how to get the number of open file descriptor
+   */
+  abstract static class OSResourceAnalyzer extends ResourceChecker.ResourceAnalyzer {
+    protected static final OperatingSystemMXBean osStats;
+    protected static final UnixOperatingSystemMXBean unixOsStats;
+
+    static {
+      osStats =
+          ManagementFactory.getOperatingSystemMXBean();
+      if (osStats instanceof UnixOperatingSystemMXBean) {
+        unixOsStats = (UnixOperatingSystemMXBean) osStats;
+      } else {
+        unixOsStats = null;
+      }
+    }
+  }
+
+  static class OpenFileDescriptorResourceAnalyzer extends OSResourceAnalyzer {
+    @Override
+    public int getVal() {
+      if (unixOsStats == null) {
+        return 0;
+      } else {
+        return (int) unixOsStats.getOpenFileDescriptorCount();
+      }
+    }
+
+    public int getMax() {
+      return 1024;
+    }
+  }
+
+  static class MaxFileDescriptorResourceAnalyzer extends OSResourceAnalyzer {
+    @Override
+    public int getVal() {
+      if (unixOsStats == null) {
+        return 0;
+      } else {
+        return (int) unixOsStats.getMaxFileDescriptorCount();
+      }
+    }
+  }
+
+
+  public ResourceCheckerJUnitListener() {
+  }
+
+  protected void addResourceAnalyzer(ResourceChecker rc) {
+  }
+
+  /**
+   * To be called before the test methods
+   *
+   * @param testName
+   */
+  private void start(String testName) {
+    ResourceChecker rc = new ResourceChecker(testName);
+    rc.addResourceAnalyzer(new ThreadResourceAnalyzer());
+    rc.addResourceAnalyzer(new OpenFileDescriptorResourceAnalyzer());
+    rc.addResourceAnalyzer(new MaxFileDescriptorResourceAnalyzer());
+
+    addResourceAnalyzer(rc);
+
+    rcs.put(testName, rc);
+
+    rc.start();
+  }
+
+  /**
+   * To be called after the test methods
+   *
+   * @param testName
+   */
+  private void end(String testName) {
+    ResourceChecker rc = rcs.remove(testName);
+    assert rc != null;
+    rc.end();
+  }
+
+  /**
+   * Get the test name from the JUnit Description
+   *
+   * @return the string for the short test name
+   */
+  private String descriptionToShortTestName(
+      org.junit.runner.Description description) {
+    final int toRemove = "org.apache.hadoop.hbase.".length();
+    return description.getTestClass().getName().substring(toRemove) +
+        "#" + description.getMethodName();
+  }
+
+  public void testStarted(org.junit.runner.Description description) throws java.lang.Exception {
+    start(descriptionToShortTestName(description));
+  }
+
+  public void testFinished(org.junit.runner.Description description) throws java.lang.Exception {
+    end(descriptionToShortTestName(description));
+  }
+}
+
