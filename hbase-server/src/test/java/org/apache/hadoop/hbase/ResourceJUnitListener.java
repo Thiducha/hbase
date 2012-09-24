1bc1ee73cfa6d267a07db823f0443874d0a9aa61
Nicolas Liochon <nkeywal@gmail.com>
2012-09-24 13:44:30 +0200

    Merge branch 'clean' into 6364
    Conflicts:
    	hbase-server/src/main/java/org/apache/hadoop/hbase/ipc/ClientCache.java

--- hbase-server/src/test/java/org/apache/hadoop/hbase/ResourceJUnitListener.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/ResourceJUnitListener.java
@@ -7,8 +7,12 @@ import org.junit.runner.notification.RunListener;
 import java.lang.management.ManagementFactory;
 
 public class ResourceJUnitListener extends RunListener {
-  public ResourceJUnitListener(){
-    System.err.println("AAAAAAAAAAAAAAAA ResourceJUnitListener  "+ManagementFactory.getRuntimeMXBean().getName() );
+  private String processId = ManagementFactory.getRuntimeMXBean().getName();
+  private ResourceChecker cu;
+  private boolean endDone;
+
+  public ResourceJUnitListener() {
+    System.err.println("AAAAAAAAAAAAAAAA ResourceJUnitListener  ");
   }
 
   /**
@@ -17,6 +21,9 @@ public class ResourceJUnitListener extends RunListener {
    * @param testName
    */
   private void start(String testName) {
+    cu = new ResourceChecker("before " + testName);
+    endDone = false;
+
   }
 
   /**
@@ -25,6 +32,11 @@ public class ResourceJUnitListener extends RunListener {
    * @param testName
    */
   private void end(String testName) {
+    if (!endDone) {
+      endDone = true;
+      cu.logInfo("after " + testName);
+      cu.check("after " + testName);
+    }
   }
 
   /**
@@ -36,39 +48,40 @@ public class ResourceJUnitListener extends RunListener {
   private String descriptionToShortTestName(
       org.junit.runner.Description description) {
     final int toRemove = "org.apache.hadoop.hbase.".length();
-    System.err.println("" + description);
-    return null;// description.getTestClass().getName().substring(toRemove) + "#" + description.getMethodName();
+    return description.getTestClass().getName().substring(toRemove) +
+        "#" + description.getMethodName();
   }
 
   public void testRunStarted(org.junit.runner.Description description) throws java.lang.Exception {
-    System.err.println("AAAAAAAAAAAAAAAA testRunStarted  "+ManagementFactory.getRuntimeMXBean().getName() );
-
+    System.err.println("AAAAAAAAAAAAAAAA testRunStarted  " + ManagementFactory.getRuntimeMXBean().getName());
   }
 
   public void testRunFinished(org.junit.runner.Result result) throws java.lang.Exception {
-    System.err.println("AAAAAAAAAAAAAAAAA testRunFinished  "+ManagementFactory.getRuntimeMXBean().getName() );
+    System.err.println("AAAAAAAAAAAAAAAAA testRunFinished  " + ManagementFactory.getRuntimeMXBean().getName());
 
   }
 
   public void testStarted(org.junit.runner.Description description) throws java.lang.Exception {
-    System.err.println("AAAAAAAAAAAAAAAA testStarted  "+ManagementFactory.getRuntimeMXBean().getName());
+    System.err.println("AAAAAAAAAAAAAAAA testStarted  " + ManagementFactory.getRuntimeMXBean().getName());
+    start(descriptionToShortTestName(description));
   }
 
   public void testFinished(org.junit.runner.Description description) throws java.lang.Exception {
-    System.err.println("AAAAAAAAAAAAAAAA testFinished  "+ManagementFactory.getRuntimeMXBean().getName() );
+    System.err.println("AAAAAAAAAAAAAAAA testFinished  " + ManagementFactory.getRuntimeMXBean().getName());
+    end(descriptionToShortTestName(description));
   }
 
   public void testFailure(org.junit.runner.notification.Failure failure) throws java.lang.Exception {
-    System.err.println("AAAAAAAAAAAAAAAA testFailure  "+ManagementFactory.getRuntimeMXBean().getName());
+    System.err.println("AAAAAAAAAAAAAAAA testFailure  " + ManagementFactory.getRuntimeMXBean().getName());
   }
 
   public void testAssumptionFailure(org.junit.runner.notification.Failure failure) {
-    System.err.println("AAAAAAAAAAAAAAAA testAssumptionFailure  "+ManagementFactory.getRuntimeMXBean().getName() );
+    System.err.println("AAAAAAAAAAAAAAAA testAssumptionFailure  " + ManagementFactory.getRuntimeMXBean().getName());
 
   }
 
   public void testIgnored(org.junit.runner.Description description) throws java.lang.Exception {
-    System.err.println("AAAAAAAAAAAAAAAA testIgnored  " );
+    System.err.println("AAAAAAAAAAAAAAAA testIgnored  ");
   }
 }
 
