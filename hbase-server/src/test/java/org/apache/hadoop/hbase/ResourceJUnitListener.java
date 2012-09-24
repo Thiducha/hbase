20cd3756b257cd2dad53e2980b180c0e5a329117
Nicolas Liochon <nkeywal@gmail.com>
2012-09-24 12:06:48 +0200

    Merge branch 'clean' into 6364
    Conflicts:
    	hbase-server/src/main/java/org/apache/hadoop/hbase/ipc/ClientCache.java

---
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/ResourceJUnitListener.java
@@ -0,0 +1,68 @@
+package org.apache.hadoop.hbase;
+
+
+import org.junit.runner.notification.RunListener;
+
+public class ResourceJUnitListener extends RunListener {
+  private ResourceChecker cu;
+  private boolean endDone;
+
+  /**
+   * To be called before the test methods
+   *
+   * @param testName
+   */
+  private void start(String testName) {
+    cu = new ResourceChecker("before " + testName);
+    endDone = false;
+  }
+
+  /**
+   * To be called after the test methods
+   *
+   * @param testName
+   */
+  private void end(String testName) {
+    if (!endDone) {
+      endDone = true;
+      cu.logInfo("after " + testName);
+      cu.check("after " + testName);
+    }
+  }
+
+  /**
+   * Get the test name from the JUnit Description
+   *
+   * @param description
+   * @return the string for the short test name
+   */
+  private String descriptionToShortTestName(
+      org.junit.runner.Description description) {
+    final int toRemove = "org.apache.hadoop.hbase.".length();
+    return description.getTestClass().getName().substring(toRemove) +
+        "#" + description.getMethodName();
+  }
+
+  public void testRunStarted(org.junit.runner.Description description) throws java.lang.Exception {
+  }
+
+  public void testRunFinished(org.junit.runner.Result result) throws java.lang.Exception {
+  }
+
+  public void testStarted(org.junit.runner.Description description) throws java.lang.Exception {
+    start(descriptionToShortTestName(description));
+  }
+
+  public void testFinished(org.junit.runner.Description description) throws java.lang.Exception {
+    end(descriptionToShortTestName(description));
+  }
+
+  public void testFailure(org.junit.runner.notification.Failure failure) throws java.lang.Exception {
+  }
+
+  public void testAssumptionFailure(org.junit.runner.notification.Failure failure) {
+  }
+
+  public void testIgnored(org.junit.runner.Description description) throws java.lang.Exception {
+  }
+}
