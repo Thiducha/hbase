10b5573e8afe709a6ec08eb7d0591d0c43c2d1c4
Nicolas Liochon <nkeywal@gmail.com>
2012-11-30 17:27:32 +0100

    4709

--- hbase-server/src/main/java/org/apache/hadoop/hbase/master/AssignmentManager.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/master/AssignmentManager.java
@@ -982,12 +982,6 @@ public class AssignmentManager extends ZooKeeperListener {
    */
   @Override
   public void nodeChildrenChanged(String path) {
-    try {
-      ZKUtil.listChildrenAndWatchForNewChildren(
-          watcher, watcher.assignmentZNode);
-    } catch (KeeperException e) {
-
-    }
     if (true) return;
     if (path.equals(watcher.assignmentZNode)) {
       int wi = Math.abs(path.hashCode() % zkEventWorkers.length);
