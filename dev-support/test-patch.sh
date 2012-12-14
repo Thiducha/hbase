66806086ea9baa0205353b44f60ad23c8a51542d
Nicolas Liochon <nkeywal@gmail.com>
2012-12-14 11:04:29 +0100

    4709

--- dev-support/test-patch.sh
+++ dev-support/test-patch.sh
@@ -634,10 +634,10 @@ runTests () {
     BAD=0
   fi
   ZOMBIE_TESTS_COUNT=`jps | grep surefirebooter | wc -l`
-  if [[ ZOMBIE_TESTS_COUNT" != 0 ]] ; then
+  if [[ ZOMBIE_TESTS_COUNT != 0 ]] ; then
     echo "There are $ZOMBIE_TESTS_COUNT zombie tests, that should have been killed by surefire but survived"
     echo "************ BEGIN zombies jstack extract"
-    jps |grep surefirebooter | cut -d ' ' -f 1 | xargs -n 1 jstack | grep Test
+    jps | grep surefirebooter | cut -d ' ' -f 1 | xargs -n 1 jstack | grep ".test" | grep "\.java"
     echo "************ END  zombies jstack extract"
      JIRA_COMMENT="$JIRA_COMMENT
 
