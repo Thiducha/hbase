1118129f24e7b439b21c64b6738538015e53e341
Nicolas Liochon <nkeywal@gmail.com>
2013-04-02 12:17:58 +0200

    1

--- hbase-server/src/main/java/org/apache/hadoop/hbase/util/FSHDFSUtils.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/util/FSHDFSUtils.java
@@ -26,7 +26,9 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hdfs.DistributedFileSystem;
+import org.apache.hadoop.hdfs.server.namenode.LeaseExpiredException;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InterruptedIOException;
 
@@ -34,11 +36,14 @@ import java.io.InterruptedIOException;
 /**
  * Implementation for hdfs
  */
-@InterfaceAudience.Public
+@InterfaceAudience.Private
 @InterfaceStability.Evolving
 public class FSHDFSUtils extends FSUtils{
   private static final Log LOG = LogFactory.getLog(FSHDFSUtils.class);
 
+  /**
+   * Recover the lease from HDFS, retrying multiple times.
+   */
   @Override
   public void recoverFileLease(final FileSystem fs, final Path p, Configuration conf)
       throws IOException {
@@ -51,7 +56,8 @@ public class FSHDFSUtils extends FSUtils{
     LOG.info("Recovering file " + p);
     long startWaiting = EnvironmentEdgeManager.currentTimeMillis();
     // Default is 15 minutes. It's huge, but the idea is that if we have a major issue, HDFS
-    //  usually needs 10 minutes before marking the nodes as dead.
+    //  usually needs 10 minutes before marking the nodes as dead. So we're putting ourselves
+    //  beyond that limit 'to be safe'.
     long recoveryTimeout = conf.getInt("hbase.lease.recovery.timeout", 900000) + startWaiting;
     boolean recovered = false;
     int nbAttempt = 0;
@@ -59,17 +65,23 @@ public class FSHDFSUtils extends FSUtils{
       nbAttempt++;
       try {
         // recoverLease is asynchronous. We expect it to return true at the first call if the
-        //  file is closed, false when it starts the lease recovery, true when the lease recovery
-        //  has succeeded.
+        //  file is closed. So, it returns:
+        //    - false when it starts the lease recovery (i.e. lease recovery not *yet* done
+        //    - true when the lease recovery has succeeded or the file is closed.
         recovered = dfs.recoverLease(p);
         LOG.info("Attempt " + nbAttempt + " to recoverLease on file " + p +
             " returned " + recovered + ", trying for " +
             (EnvironmentEdgeManager.currentTimeMillis() - startWaiting) + "ms");
       } catch (IOException e) {
+        if (e instanceof LeaseExpiredException && e.getMessage().contains("File does not exist")) {
+          // This exception comes out instead of FNFE, fix it
+          throw new FileNotFoundException("The given HLog wasn't found at " + p);
+        }
         LOG.warn("Got IOException on attempt " + nbAttempt + " to recover lease for file " + p +
             ", retrying.", e);
       }
       if (!recovered) {
+        // try at least twice.
         if (nbAttempt > 2 && recoveryTimeout < EnvironmentEdgeManager.currentTimeMillis()) {
           LOG.error("Can't recoverLease after " + nbAttempt + " attempts and " +
               (EnvironmentEdgeManager.currentTimeMillis() - startWaiting) + "ms " + " for " + p +
