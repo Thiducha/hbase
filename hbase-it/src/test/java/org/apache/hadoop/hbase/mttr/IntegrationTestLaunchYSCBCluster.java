060f6159940aff8b8ede8e92faafac95d3c12a79
Nicolas Liochon <nkeywal@gmail.com>
2013-04-17 12:49:49 +0200

    13

--- hbase-it/src/test/java/org/apache/hadoop/hbase/mttr/IntegrationTestLaunchYSCBCluster.java
+++ hbase-it/src/test/java/org/apache/hadoop/hbase/mttr/IntegrationTestLaunchYSCBCluster.java
@@ -1,15 +1,22 @@
 package org.apache.hadoop.hbase.mttr;
 
 
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.ClusterManager;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HConstants;
+import org.apache.hadoop.hbase.HTableDescriptor;
 import org.apache.hadoop.hbase.IntegrationTests;
+import org.apache.hadoop.hbase.client.HBaseAdmin;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.util.RegionSplitter;
+import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
 @Category(IntegrationTests.class)
 public class IntegrationTestLaunchYSCBCluster extends AbstractIntegrationTestRecovery {
 
-  @Override
-  protected void genericStart() throws Exception {
+  protected void start() throws Exception {
     // Let's start ZK immediately, it will initialize itself while the NN and the DN are starting
     hcm.start(ClusterManager.ServiceType.ZOOKEEPER, mainBox);
 
@@ -38,12 +45,23 @@ public class IntegrationTestLaunchYSCBCluster extends AbstractIntegrationTestRec
         util.getHBaseAdmin().getClusterStatus().getRegionsCount() != 1) {
       Thread.sleep(200);
     }
+  }
 
-    util.getHBaseAdmin().setBalancerRunning(false, true);
+
+  @Override
+  @Test
+  public void testKillServer() throws Exception {
+    prepareCluster();
+    start();
+    HBaseAdmin admin = util.getHBaseAdmin();
+
+    HTableDescriptor desc = new HTableDescriptor("usertable");
+    desc.addFamily(new HColumnDescriptor("family"));
+    admin.createTable(desc);
+    while (true)Thread.sleep(10000);
   }
 
   @Override
   protected void kill(String hostname) throws Exception {
-    // we don't kill
   }
 }
