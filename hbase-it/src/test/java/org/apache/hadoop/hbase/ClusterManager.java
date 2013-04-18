1879971a372fc1e03a06d0e6f36a8a6cc7ef829f
Nicolas Liochon <nkeywal@gmail.com>
2013-04-18 12:14:06 +0200

    v1.1

--- hbase-it/src/test/java/org/apache/hadoop/hbase/ClusterManager.java
+++ hbase-it/src/test/java/org/apache/hadoop/hbase/ClusterManager.java
@@ -63,7 +63,7 @@ public abstract class ClusterManager extends Configured {
     HADOOP_TASKTRACKER("tasktracker"),
     HBASE_MASTER("master"),
     HBASE_REGIONSERVER("regionserver"),
-    ZOOKEEPER("-A zookeeper");
+    ZOOKEEPER("zookeeper");
 
     private String name;
 
