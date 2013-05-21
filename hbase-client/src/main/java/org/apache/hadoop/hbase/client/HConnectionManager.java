7fda6ef5f876811eb8843e6836dfb1f5af673357
Nicolas Liochon <nkeywal@gmail.com>
2013-05-21 13:07:11 +0200

    v1.1

--- hbase-client/src/main/java/org/apache/hadoop/hbase/client/HConnectionManager.java
+++ hbase-client/src/main/java/org/apache/hadoop/hbase/client/HConnectionManager.java
@@ -2298,6 +2298,7 @@ public class HConnectionManager {
 
       /**
        * Extract from the rows list what we can submit.
+       *
        * @param rows - the rows actually taken will be removed from the list. The rows that
        *             cannot be sent (overloaded region) will be kept in the list.
        */
@@ -2349,7 +2350,7 @@ public class HConnectionManager {
 
       /**
        *
-       * @param row the row OR
+       * @param row the row
        * @param numAttempt the num attempt
        * @param posInList the position in the list
        * @param force if we must submit whatever the server load
@@ -2416,7 +2417,7 @@ public class HConnectionManager {
           actions.add(action);
         }
 
-        submit(actions, 1, true);
+        submit(actions, actions, 1, true);
       }
 
 
@@ -2424,51 +2425,47 @@ public class HConnectionManager {
        * Group a list of actions per region servers, and send them. The created MultiActions are
        * added to the inProgress list.
        *
-       * @param actions    the list of row to submit
+       * @param initialActions the full list of the actions in progress
+       * @param currentActions    the list of row to submit
        * @param numAttempt the current numAttempt (first attempt is 1)
        * @param force      true if we submit the rowList without taking into account the server load
        * @throws IOException - if we can't locate a region after multiple retries.
        */
-      private void submit(List<Action<Row>> actions, int numAttempt, boolean force) {
+      private void submit(List<Action<Row>> initialActions,
+                          List<Action<Row>> currentActions, int numAttempt, boolean force) {
         // group per location => regions server
         final Map<HRegionLocation, MultiAction<Row>> actionsByServer =
             new HashMap<HRegionLocation, MultiAction<Row>>();
-        List<Action<Row>> retainedActions =
-            new ArrayList<Action<Row>>(actions.size());
 
         // We have the same policy for a single region per call to submit: we don't want
         //  to send half of the actions because the status changed in the middle. So we keep the
         //  status
         Map<String, Boolean> regionStatus = new HashMap<String, Boolean>();
 
-        Iterator<Action<Row>> it = actions.iterator();
-        int posInList = -1;
-        while (it.hasNext()) {
-          posInList++;
-          Action<Row> action = it.next();
-          HRegionLocation loc = shouldSubmit(action.getAction(), 1, posInList, force, regionStatus);
+        for (Action<Row> action : currentActions) {
+          HRegionLocation loc = shouldSubmit(
+              action.getAction(), 1, action.getOriginalIndex(), force, regionStatus);
 
           if (loc != null) {
             addAction(loc, action, actionsByServer);
-            retainedActions.add(action);
           }
         }
 
-        sendMultiAction(retainedActions, actionsByServer, numAttempt);
+        if (!actionsByServer.isEmpty()){
+          sendMultiAction(initialActions, actionsByServer, numAttempt);
+        }
       }
 
       /**
-       *
-       * @param retainedActions
-       * @param actionsByServer
-       * @param numAttempt
+       * Send a multi action structure to the servers, after a delay depending on the attempt
+       * number. Asynchronous.
+       * @param initialActions the list of the actions, flat.
+       * @param actionsByServer the actions structured by regions
+       * @param numAttempt the attempt number.
        */
-     public void sendMultiAction(List<Action<Row>> retainedActions, Map<HRegionLocation,
-         MultiAction<Row>> actionsByServer, int numAttempt) {
-
-       if (retainedActions.isEmpty()){
-         return;
-       }
+     public void sendMultiAction(List<Action<Row>> initialActions,
+                                 Map<HRegionLocation,MultiAction<Row>> actionsByServer,
+                                 int numAttempt) {
 
         // Send the queries and add them to the inProgress list
         for (Entry<HRegionLocation, MultiAction<Row>> e : actionsByServer.entrySet()) {
@@ -2476,8 +2473,7 @@ public class HConnectionManager {
           if (numAttempt > 1) {
             backoffTime = ConnectionUtils.getPauseTime(hci.pause, numAttempt - 1);
           }
-          Runnable runnable =
-              createDelayedRunnable(retainedActions, backoffTime, e.getKey(),
+          Runnable runnable = createDelayedRunnable(initialActions, backoffTime, e.getKey(),
                   e.getValue(), numAttempt);
           if (LOG.isTraceEnabled() && numAttempt > 1) {
             StringBuilder sb = new StringBuilder();
@@ -2532,12 +2528,13 @@ public class HConnectionManager {
       }
 
       /**
-       *
-       * @param rsActions
-       * @param location
-       * @param numAttempt
+       * Resubmit all the actions from this multiaction.
+       * @param rsActions the actions
+       * @param location the destination
+       * @param numAttempt the number of attemp so far
        */
-      private void resubmitAll(MultiAction<Row> rsActions, HRegionLocation location, int numAttempt) {
+      private void resubmitAll(List<Action<Row>> initialActions, MultiAction<Row> rsActions,
+                               HRegionLocation location, int numAttempt) {
         List<Action<Row>> toReplay = new ArrayList<Action<Row>>();
         for (List<Action<Row>> actions : rsActions.actions.values()) {
           for (Action<Row> action : actions) {
@@ -2550,12 +2547,12 @@ public class HConnectionManager {
             }
           }
         }
-        submit(toReplay, numAttempt, true);
+        submit(initialActions, toReplay, numAttempt, true);
       }
 
       /**
        * Called when we receive the result of a server query.
-       * @param originalActionsList - the whole action list
+       * @param initialActions - the whole action list
        * @param rsActions - the actions for this location
        * @param location - the location
        * @param responses - the response, if any
@@ -2563,14 +2560,14 @@ public class HConnectionManager {
        * @throws InterruptedException
        * @throws IOException
        */
-      private void receiveMultiAction(List<Action<Row>> originalActionsList,
+      private void receiveMultiAction(List<Action<Row>> initialActions,
                                       MultiAction<Row> rsActions, HRegionLocation location,
                                       MultiResponse responses,int numAttempt) {
 
         if (responses == null) {
           LOG.info("Attempt #" + numAttempt + " failed for all operations on server " +
               location.getServerName() + " , trying to resubmit.");
-          resubmitAll(rsActions, location, numAttempt + 1);
+          resubmitAll(initialActions, rsActions, location, numAttempt + 1);
           return;
         }
 
@@ -2585,13 +2582,14 @@ public class HConnectionManager {
         int failureCount = 0;
         for (Entry<byte[], List<Pair<Integer, Object>>> resultsForRS :
             responses.getResults().entrySet()) {
+
           for (Pair<Integer, Object> regionResult : resultsForRS.getValue()) {
             Object result = regionResult.getSecond();
 
             // Failure: retry if it's make sense else update the errors lists
             if (result == null || result instanceof Throwable) {
               failureCount++;
-              Action<Row> correspondingAction = originalActionsList.get(regionResult.getFirst());
+              Action<Row> correspondingAction = initialActions.get(regionResult.getFirst());
               Row row = correspondingAction.getAction();
               hci.updateCachedLocations(this.tableName, row, result, location);
 
@@ -2601,7 +2599,7 @@ public class HConnectionManager {
               }
             } else // success
               if (callback != null) {
-                Action<Row> correspondingAction = originalActionsList.get(regionResult.getFirst());
+                Action<Row> correspondingAction = initialActions.get(regionResult.getFirst());
                 Row row = correspondingAction.getAction();
                 this.callback.success(correspondingAction.getOriginalIndex(),
                     resultsForRS.getKey(), row.getRow(), (Res) result);
@@ -2613,7 +2611,7 @@ public class HConnectionManager {
           LOG.info("Attempt #" + numAttempt + " failed for " + failureCount +
               "operations on server " + location.getServerName() + " , resubmitting " +
               toReplay.size() + ", tableName=" + Bytes.toString(tableName));
-          submit(toReplay, numAttempt + 1, true);
+          submit(initialActions, toReplay, numAttempt + 1, true);
         } else if (failureCount != 0) {
           LOG.warn("Attempt #" + numAttempt + " failed for " + failureCount +
               "operations on server " + location.getServerName() + " NOT resubmitting." +
@@ -2706,15 +2704,15 @@ public class HConnectionManager {
       /**
        * Create a specific Runnable that will wait a given amount of ms before executing the
        *  call to the region servers.
-       * @param originalActionsList
+       * @param initialActions
        * @param delay - how long to wait
        * @param loc - location
        * @param multi - the multi action to execute
        * @param numAttempt - the number of attempt
        * @return the Runnable
        */
-      private Runnable createDelayedRunnable(
-          final List<Action<Row>> originalActionsList, final long delay, final HRegionLocation loc,
+      private Runnable createDelayedRunnable(final List<Action<Row>> initialActions,
+                                             final long delay, final HRegionLocation loc,
           final MultiAction<Row> multi, final int numAttempt) {
 
         final Callable<MultiResponse> delegate = hci.createCallable(loc, multi, tableName);
@@ -2734,11 +2732,11 @@ public class HConnectionManager {
                   Thread.sleep(waitingTime);
                 } catch (InterruptedException e) {
                   LOG.warn("We've been interrupted while waiting to send" +
-                      " for regionName=" + regionName);
+                      " for regionName=" + regionName);    /*
                   for (Action<? extends Row> action:originalActionsList){
                     manageError(numAttempt, action.getOriginalIndex(),
                         action.getAction(), false, e, loc);
-                  }
+                  }       */
                   Thread.interrupted();
                   return;
                 }
@@ -2749,13 +2747,16 @@ public class HConnectionManager {
               } catch (Exception e) {
                 LOG.warn("The call to the RS failed, we don't know where we stand. regionName="
                     + regionName, e);
+
+                             /*
                 for (Action<? extends Row> action:originalActionsList){
                   manageError(numAttempt, action.getOriginalIndex(),
                       action.getAction(), false, e, loc);
-                }
+                }          */
+
                 return;
               }
-              receiveMultiAction(originalActionsList, multi, loc, res, numAttempt);
+              receiveMultiAction(initialActions, multi, loc, res, numAttempt);
             } finally {
               decTaskCounters(regionName);
             }
